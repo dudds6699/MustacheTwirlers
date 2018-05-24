b9fbd926c3fe0527e8728ac7968a29320340c973
Chris Bodley <dudds6699@hotmail.com>
2018-05-24 16:45:16 -0400

    added firebase js to it.

--- MustacheTwirlers/www/scripts/index.js
+++ MustacheTwirlers/www/scripts/index.js
@@ -2,6 +2,8 @@
 // http://go.microsoft.com/fwlink/?LinkID=397704
 // To debug code on page load in cordova-simulate or on Android devices/emulators: launch your app, set breakpoints, 
 // and then run "window.location.reload()" in the JavaScript Console.
+
+
 (function () {
     "use strict";
 
